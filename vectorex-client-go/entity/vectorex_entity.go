93d298d58e7441284f0367b25a5a646138f82b61
xgc <javpower@163.com>
2025-01-10 11:39:08 +0800

    add go sdk

--- vectorex-client-go/entity/vectorex_entity.go
+++ vectorex-client-go/entity/vectorex_entity.go
@@ -2,6 +2,6 @@ package entity
 
 type VectoRexEntity struct {
 	CollectionName string                          `json:"collectionName"`
-	VectorFields   []KeyValue[string, VectorField] `json:"vectorFields"`
+	VectorFields   []KeyValue[string, VectorField] `json:"vectorFileds"`
 	ScalarFields   []KeyValue[string, ScalarField] `json:"scalarFields"`
 }
